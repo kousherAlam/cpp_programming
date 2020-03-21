b67bdd1d30237d6ed4fc043c05cda3a3b905554b
kousheralam <kousheralampranto@gmail.com>
2020-03-21 14:15:53 +0600

    added some conditional logic based on os

--- README.md
+++ README.md
@@ -10,3 +10,19 @@
 
 #### Using multiple compiler 
 
+- `cmake_minimum_required`: declare the minimum version for cmake 
+
+- `project`: specify project and version name..
+
+- 
+
+
+#### Variable to check system related info 
+- CMAKE_SYSTEM 
+- CMAKE_SYSTEM_NAME
+- CMAKE_SYSTEM_VERSION
+- CMAKE_SYSTEM_PROCESSOR
+- CMAKE_HOST_SYSTEM_NAME 
+- `UNIX`, `WIN32`, `WINRT`, `CYGWIN`, `APPLE` are environment variables as flags set by default system
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,27 @@
 #include "./persons/src/Banker.h"
 #include "./services/users/user.service.h"
 
-int main(){
+#ifndef CMAKE_MACRO_SAMPLE
+    #define CMAKE_MACRO_SAMPLE "NO SYSTEM NAME"
+#endif
+
+int main()
+{
+    std::cout 
+            << "System Name: "
+            << CMAKE_MACRO_SAMPLE 
+            << std::endl
+            << std::endl;
     UserService us;
 
-    const Person * p2 = us.getAllPerson();
+    const Person *p2 = us.getAllPerson();
 
     Person first = p2[0];
 
-
     Person p;
     Banker b;
-
+    std::cout << CMAKE_MACRO_SAMPLE << std::endl;
     std::cout << "hello world ..." << std::endl;
-
 
     return 0;
 }
-
